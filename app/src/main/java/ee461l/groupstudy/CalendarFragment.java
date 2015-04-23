dcbd9d51f29166d9a661a68ce3614b4d041c80c6
rjm2932 <ryanmccluretx@gmail.com>
2015-04-23 01:01:58 -0500

    Calendar date and time

--- app/src/main/java/ee461l/groupstudy/CalendarFragment.java
+++ app/src/main/java/ee461l/groupstudy/CalendarFragment.java
@@ -1,14 +1,21 @@
 package ee461l.groupstudy;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 
 import java.util.ArrayList;
@@ -20,8 +27,12 @@ public class CalendarFragment extends Fragment implements View.OnClickListener {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     public static final String ARG_MENU_CHOICE_NUM = "menu_number";
     UserList userList;
-    TextView users;
-    TextView event;
+    static TextView users; //**maybe shouldnt be static
+    TextView eventDate;
+    TextView eventTime;
+    ArrayList eventList = new ArrayList<>(); //
+    FragmentManager manager;
+    private String description_Text = "";
 
     // TODO: Rename and change types of parameters
     private String menuChoice;
@@ -60,20 +71,22 @@ public class CalendarFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_home_page, container, false);
-        ArrayList lists = new ArrayList<>();
-        lists.add("04/20/15 5pm: Meeting 1 at ENS");
-        lists.add("04/21/15 3pm: Meeting 2 at PCL");
-        lists.add("04/23/15 12:30pm: Meeting 3 at ECJ");
+
+        eventList.add("04/20/15 5pm: Meeting 1 at ENS");
+        eventList.add("04/21/15 3pm: Meeting 2 at PCL");
+        eventList.add("04/23/15 12:30pm: Meeting 3 at ECJ");
 
         View rootView = inflater.inflate(R.layout.fragment_calendar, container, false);
         Button newEvent = (Button)rootView.findViewById(R.id.newEvent);
         newEvent.setOnClickListener(this);
         users = (TextView) rootView.findViewById(R.id.users);
-        event = (TextView) rootView.findViewById(R.id.event);
-        event.setVisibility(View.GONE);
+        eventDate = (TextView) rootView.findViewById(R.id.eventDate);
+        eventDate.setVisibility(View.GONE);
+        eventTime = (TextView) rootView.findViewById(R.id.eventTime);
+        eventTime.setVisibility(View.GONE);
         //users.setText("Hello everyone");
-        users.setText(lists.get(0).toString());
-        users.append("\n" + lists.get(1).toString() + "\n" + lists.get(2).toString());
+        users.setText(eventList.get(0).toString());
+        users.append("\n" + eventList.get(1).toString() + "\n" + eventList.get(2).toString());
         getActivity().setTitle("Calendar");
 
         return rootView;
@@ -85,11 +98,28 @@ public class CalendarFragment extends Fragment implements View.OnClickListener {
         int mYear = c.get(Calendar.YEAR);
         int mMonth = c.get(Calendar.MONTH);
         int mDay = c.get(Calendar.DAY_OF_MONTH);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        int AmPm = c.get(Calendar.AM_PM);
+
         //users.append("the selected " + mDay);
         DatePickerDialog dialog = new DatePickerDialog(getActivity(),
                 new mDateSetListener(), mYear, mMonth, mDay);
+
+        //DialogFragment newFragment = new TimePickerFragment();
+        //newFragment.show(this.manager, "timePicker");
+        TimePickerDialog timeDialog = new TimePickerDialog(getActivity(),
+                new mTimeSetListener(), hour, minute, false);
+        //Dialog description = new Dialog(getActivity());
+        //description.show();
+        timeDialog.show();
         dialog.show();
+
+
     }
+
+
+
 
     class mDateSetListener implements DatePickerDialog.OnDateSetListener {
 
@@ -102,13 +132,43 @@ public class CalendarFragment extends Fragment implements View.OnClickListener {
             int mMonth = monthOfYear;
             int mDay = dayOfMonth;
 
-            event.setText(new StringBuilder()
+            eventDate.setText(new StringBuilder()
                     // Month is 0 based so add 1
                     .append(mMonth + 1).append("/").append(mDay).append("/")
                     .append(mYear).append(" "));
-            users.append("\n" + event.getText().toString());
+            eventList.add(eventDate.getText().toString());
+            int index = eventList.indexOf(eventDate.getText().toString());
+            users.append("\n" + eventList.get(index).toString());
 
+            //DialogFragment tpf = new TimePickerFragment();
+            //DialogFragment newFragment = new TimePickerFragment();
+            //newFragment.show(manager, "timePicker");
 
         }
     }
+
+    class mTimeSetListener implements TimePickerDialog.OnTimeSetListener{
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            int mHour = hourOfDay;
+            int mMinute = minute;
+
+            eventTime.setText(new StringBuilder()
+                    .append(mHour).append(":").append(mMinute));
+            eventList.add(eventTime.getText().toString());
+            int index = eventList.indexOf(eventTime.getText().toString());
+            users.append(" " + eventList.get(index).toString());
+        }
+    }
+
+    class mDialogListener implements Dialog.OnCancelListener{
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            String description = dialog.toString();
+            users.append(" " + description);
+        }
+    }
+
 }
